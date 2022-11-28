fd01e9a4775d44ad008b8c7807bf8c183cbe000b
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-11-28 16:46:16 +0700

    create method public void display

--- StackUsingLinkList/Program.cs
+++ StackUsingLinkList/Program.cs
@@ -40,8 +40,34 @@ namespace StackUsingLinkList
             top = fresh;
             Console.WriteLine("\n" + element + "pushed");
         }
-        
 
+        public void pop()
+        {
+            Console.WriteLine("\n The poped elements is: " + top.info);
+            top = top.next; // make top point to the next node is sequence
+
+        }
+
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp !=null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+
+            }
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        stack s = new stack();
     }
     class Program
     {
