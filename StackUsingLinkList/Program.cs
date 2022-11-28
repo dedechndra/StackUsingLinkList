72f10fb6e78e0cfc688e6c3a95d7721eed825699
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-11-28 16:59:45 +0700

    create method static void main string

--- StackUsingLinkList/Program.cs
+++ StackUsingLinkList/Program.cs
@@ -68,12 +68,42 @@ namespace StackUsingLinkList
     static void Main(string[] args)
     {
         stack s = new stack();
-    }
-    class Program
-    {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("\n***Stack Menu***\n");
+            Console.WriteLine("1. Push ");
+            Console.WriteLine("2. Pop ");
+            Console.WriteLine("3. Display ");
+            Console.WriteLine("4. exit");
+            Console.WriteLine("\nEnter your choice ");
+            string Input = Console.ReadLine();
+            char ch = Convert.ToChar(Input == "" ? "0" : Input);
+            switch (ch)
+            {
+                case '1':
+                    Console.Write("\nEnter a number :");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    s.push(num);
+                    break;
+                case '2':
+                    if (s.empty())
+                    {
+                        Console.WriteLine("\nSTack empty");
+                        break;
+
+                    }
+                    s.pop();
+                    break;
+                case '3';
+                    s.display();
+                case '4':
+                    return;
+                default:
+                    Console.WriteLine("\nInvalid Choice");
+                    break;
+
+            }
         }
     }
 }
