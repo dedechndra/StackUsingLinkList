9f72f493b069ebae19c4ca5bb23bc0b974951570
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-11-28 16:24:30 +0700

    Add project files.

---
+++ StackUsingLinkList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StackUsingLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n; 
+        }
+    }
+    class stack
+    {
+        node top;
+        public stack()
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
